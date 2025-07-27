c8e8f97a7aed4d903bd80db9a30005d7b637d755
Kris-Polyakova <kris1617polyakova@yandex.ru>
2025-07-27 22:29:16 +0700

    Update task.js

--- ads/task.js
+++ ads/task.js
@@ -2,9 +2,9 @@ const messages = document.querySelectorAll('.rotator__case');
 let currentIndex = 0;
 
 function rotation() {
-    messages[currentIndex].classList.remove('rotator__case_active')
+    messages[currentIndex].classList.remove('rotator__case_active');
     currentIndex = (currentIndex + 1) % messages.length;
-    messages[currentIndex].classList.add('rotator__case_active')
+    messages[currentIndex].classList.add('rotator__case_active');
 }
 
 setInterval(() => {
