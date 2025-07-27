68d6d4afe9255a37f1f6f43d5dbca8501a4cf97f
Kris-Polyakova <kris1617polyakova@yandex.ru>
2025-07-27 22:29:51 +0700

    Update task.js

--- book-reader/task.js
+++ book-reader/task.js
@@ -1,5 +1,5 @@
-const sizeButtons = document.querySelectorAll('.font-size')
-const book = document.querySelector('.book')
+const sizeButtons = document.querySelectorAll('.font-size');
+const book = document.querySelector('.book');
 
 sizeButtons.forEach(btn => {
     btn.addEventListener('click', (event) => {
@@ -8,7 +8,7 @@ sizeButtons.forEach(btn => {
 
         event.currentTarget.classList.add('font-size_active');
         size = event.currentTarget.dataset.size;
-        book.classList.remove('book_fs-big', 'book_fs-small')
+        book.classList.remove('book_fs-big', 'book_fs-small');
 
         if(size != undefined){
             book.classList.add(`book_fs-${size}`);
